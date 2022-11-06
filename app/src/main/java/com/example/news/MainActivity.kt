d9e1dcc98ed88dd8e42dd25026061c5938821f4e
Badri Akkala <lcb2021029@iiitl.ac.in>
2022-11-06 22:42:24 +0530

    step 4: entered data in main activity->initialized recyclerview->created arraylist->set adapter of recyclerview with the created arraylist

--- app/src/main/java/com/example/news/MainActivity.kt
+++ app/src/main/java/com/example/news/MainActivity.kt
@@ -2,10 +2,63 @@ package com.example.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var newRecyclerView: RecyclerView
+    private lateinit var newArrayList : ArrayList<News>
+    lateinit var imageId : Array<Int>
+    lateinit var heading : Array<String>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageId = arrayOf(
+            R.drawable.a,
+            R.drawable.a,
+            R.drawable.a,
+            R.drawable.a,
+            R.drawable.a,
+            R.drawable.a,
+            R.drawable.a,
+            R.drawable.a,
+            R.drawable.a,
+            R.drawable.a,
+            R.drawable.a,
+            R.drawable.a,
+        )
+        heading = arrayOf(
+            "1st giga chad badri ",
+            "2nd giga chad badri",
+            "3rd giga chad badri",
+            "4th giga chad badri",
+            "5th giga chad badri",
+            "6th giga chad badri",
+            "7th giga chad badri",
+            "8th giga chad badri",
+            "9th giga chad badri",
+            "10th giga chad badri",
+            "11th giga chad badri",
+            "12th giga chad badri",
+        )
+        newRecyclerView = findViewById(R.id.recyclerView)
+        newRecyclerView.layoutManager = LinearLayoutManager(this)
+        newRecyclerView.setHasFixedSize(true)
+
+        newArrayList= arrayListOf<News>()
+        getUserData()
     }
+
+    private fun getUserData(){
+        for(i in imageId.indices){
+            val news = News(imageId[i],heading[i])//creatd a single news
+            newArrayList.add(news)//adding the created single news to the newArrayList
+        }
+        newRecyclerView.adapter = MyAdapter(newArrayList)
+    }
+
 }
